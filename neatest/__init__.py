90093c1fcccea4ad258c1567ee50e679422c1a55
rtmigo <art.galkin@gmail.com>
2021-04-24 02:20:43 +0300

    initial

--- neatest/__init__.py
+++ neatest/__init__.py
@@ -1,4 +1,45 @@
 # SPDX-FileCopyrightText: (c) 2021 Artёm IG <github.com/rtmigo>
 # SPDX-License-Identifier: MIT
 
-from .neatest import run, suite, pattern, deps
+
+import subprocess
+import sys
+from typing import List, Optional
+import unittest
+from pathlib import Path
+
+pattern: str = 'test*.py'
+deps: Optional[List[str]] = None
+top_level_dir = "."
+
+
+def suite() -> unittest.TestSuite:
+    """Can be imported into `setup.py` as `test_suite="test_unit.suite"`.
+    But sadly it's deprecated."""
+
+    #print(__)
+
+    init_py = next(Path(top_level_dir).rglob("__init__.py"), None)
+    if not init_py:
+        print('__init__.py not found')
+        exit(3)
+
+    return unittest.TestLoader().discover(
+        top_level_dir=top_level_dir,
+        start_dir=str(init_py.parent),
+        pattern=pattern)
+
+
+def run() -> unittest.TestResult:
+    """Discovers and runs unit tests for the module."""
+
+    if deps:
+        if subprocess.call([sys.executable, "-m", "pip", "install"] + deps) != 0:
+            exit(1)
+
+    result = unittest.TextTestRunner(buffer=True).run(suite())
+
+    if result.failures or result.errors:
+        exit(2)
+
+    return result
---
+++ test_unit.py
@@ -0,0 +1,8 @@
+import neatest
+
+
+if __name__ == "__main__":
+    assert neatest.run().testsRun == 5
+    assert neatest.run().testsRun == 5
+    neatest.pattern = "*.py"
+    assert neatest.run().testsRun == 7
