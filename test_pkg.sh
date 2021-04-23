8e5df60cfcf8c08c53a679628cd5145eb258d26c
rtmigo <art.galkin@gmail.com>
2021-04-24 01:53:08 +0300

    initial

---
+++ test_pkg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e && source pyrel.sh
+
+# build package, install it into virtual
+# environment with pip
+pyrel_test_begin
+
+# check, that we can import this module by name
+# (so it's installed)
+python3 -c "import neatest"
+
+# remove generated package
+pyrel_test_end
